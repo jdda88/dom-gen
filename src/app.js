1e3b147506dbb36105b4b9ba5194bbcf1ff40c98
Juan David <jdda886@gmail.com>
2022-10-15 15:02:26 +0000

    gendom

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  //new array to include a random doamin to the domain(.us.io) generator
+  let dom = ["uk", "cl", "us", "io"];
+  //variable to store and add as a list our generated domain
+  let s1 = "";
+  //nested loops for each array to generate a randon part for the domain generator
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          //empty variable wich will add the elements form the loop "+=" the "\n" creates a new line of generated domain
+          s1 += pronoun[i] + adj[j] + noun[k] + "." + dom[l] + "\n";
+          //console.log(pronoun[i] + adj[j] + noun[k] + "." + dom[l]);
+        }
+      }
+    }
+  }
+  //we assigned were to print our generated domains and assigned the HTTP file
+  document.querySelector("#domGen").innerHTML = s1;
 };
